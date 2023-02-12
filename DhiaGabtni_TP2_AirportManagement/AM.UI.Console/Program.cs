29bd6ee163c0780786de6eb708e236c35c554cbd
GabtniDhia <dhia.gabtni@esprit.tn>
2023-02-12 21:18:32 +0100

    Rendu TP 2

---
+++ DhiaGabtni_TP2_AirportManagement/AM.UI.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Services;
+
+
+ServiceFlight sf = new AM.ApplicationCore.Services.ServiceFlight();
+
+sf.Flights = TestData.flights;
+
+Console.Write(sf.Flights);
+Console.WriteLine("Hello World");
+sf.GetFlights("Destination","Paris");
+
+
+
